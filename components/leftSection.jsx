4b79296cfb1eff36755cbe34979e19663a5596e4
Keval Gohil <kg256853@gmail.com>
2024-04-07 01:13:12 +0530

    first

--- components/leftSection.jsx
+++ components/leftSection.jsx
@@ -10,7 +10,16 @@ const AuthLeftSection = () => {
             <FormComponent />
           </div>
           
-          <div className="left-sec-content"></div>
+          <div className="left-sec-content left-sec-2">
+            <hr className="hr-text" data-content="Socials"></hr>
+            <div className="social-btn">
+              <a href="https://www.instagram.com/keval_gohil_1" target='_blank'><i className='ant-design--instagram-outlined'></i> Instagram</a>
+              <a href="https://www.linkedin.com/in/keval-gohil" target='_blank'><i className='circum--linkedin'></i> LinkedIn</a>
+              <a href="https://github.com/keval-gohil" target='_blank'><i className='mdi--github'></i> GitHub</a>
+              <a href="https://twitter.com/keval_gohil_1" target='_blank'><i className='ri--twitter-x-fill'></i> Twitter</a>
+              <a href="https://www.fiverr.com/keval_gohil" target='_blank'><i className='jam--fiverr'></i> Fiverr</a>
+            </div>
+          </div>
         </div>
     </>
   )
