3d3698a244cee01128876dec0cbb51377c7119a2
Keval Gohil <kg256853@gmail.com>
2024-05-09 12:07:43 +0530

    let me push

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MainHeader from '@/components/mainHeader'
 import MainSection from '@/components/mainSection';
 import '@/components/style.css'
-import '@/components/responsive.css?var=2.6'
+import '@/components/responsive.css?var=2.7'
 
 
 const Author = () => {
