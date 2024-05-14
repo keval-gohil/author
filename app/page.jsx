c99b7fdf51a6bb8eb4f15e44153940159e979a66
Keval Gohil <kg256853@gmail.com>
2024-05-14 12:32:14 +0530

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MainHeader from '@/components/mainHeader'
 import MainSection from '@/components/mainSection';
 import '@/components/style.css'
-import '@/components/responsive.css?var=2.7'
+import '@/components/responsive.css?var=2.8'
 
 
 const Author = () => {
